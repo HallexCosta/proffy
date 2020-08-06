a9374fcb2ded51d475a64b67f362851681f7b4ef
HallexCosta <hallex.costa@hotmail.com>
2020-08-06 15:55:55 -0300

    Finish component TeacherItem

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,60 @@ import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import api from '../../services/api'
+
 import './styles.css'
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+    id: number
+    avatar: string
+    bio: string
+    cost: number
+    name: string
+    subject: string
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({
+    teacher
+}) => {
+    function handleCreateNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/55293671?s=460&u=053dc2155e236f8590943d52bce9fea037b001f7&v=4" alt="Hállex da Silva Costa" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Hállex da Silva Costa</strong>
-                    <span>Desenvolvimento Web</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Estudante de Ánalise e Desenvolvimento de Sistemas, entuasiasta em desenvolvimento Web FullStack com foco no back-end.
-                <br /><br />
-                Apaixonado pelas tecnologias NodeJS, ReactJS e ReactNative. Amante das boas pratícas sendo elas Clean Architecture, Clean Code e o padrão Package by Feature, visando sempre seguir os princípios de design aplicados pelo S.O.L.I.D, K.I.S.S entre outros princípios.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora:
-                    <strong>R$ 100,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+
+                <a 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={handleCreateNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
