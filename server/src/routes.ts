ba0fea0bd6b29e0ed52f8c2460eb5dbc53b034f2
HallexCosta <hallex.costa@hotmail.com>
2020-09-01 19:00:08 -0300

    Other files back-end

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,23 @@
 import express, { Router} from 'express'
+
 import ClassesController from './controllers/ClassesController'
 import ConnectionsController from './controllers/ConnectionsController'
-
+import AccountsController from './controllers/AccountsController'
+import AuthentificationController from './controllers/AuthentificationController'
+import AuthMiddleware from './middlewares/auth'
 
 const routes: Router = express.Router()
 
 const classesController = new ClassesController
 const connectionsController = new ConnectionsController
+const accountsController = new AccountsController
+const authentificationController = new AuthentificationController
+const authMiddleware = new AuthMiddleware
+
+routes.post('/users', accountsController.validateAccount, accountsController.create)
+
+routes.post('/auth', authentificationController.findUserByEmailAndPassword)
+routes.use(authMiddleware.auth);
 
 routes.get('/classes', classesController.index)
 routes.post('/classes', classesController.create)
