d4860abc31765ab243deba6a747afca206e40a35
HallexCosta <hallex.costa@hotmail.com>
2020-09-01 18:49:37 -0300

    Creating new layout for Landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
+import {
+    Grid,
+    Header,
+    Avatar,
+    SignOutButton,
+    PowerOffIcon,
+    BodyContainer,
+    LogoContainer,
+    LandingContainer,
+    Footer,
+    WelcomeMessage,
+    ConnectionInfo,
+    ButtonsContainer
+} from './styles'
+
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 
@@ -9,10 +24,13 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
 import api from '../../services/api'
+import { useAuth } from '../../contexts/auth'
 
 import './styles.css'
 
 const Landing: React.FC = () => {
+    const { user, signOut } = useAuth()
+
     const [totalConnections, setTotalConnections] = useState<number>(0)
 
     useEffect(() => {
@@ -24,38 +42,88 @@ const Landing: React.FC = () => {
             })
     }, [])
 
+    function handleSignOut() {
+        signOut()
+    }
+
     return (
-        <div id="page-landing">
-            <div id="page-landing-content" className="container">
-                <div className="logo-container">
+        <Grid>
+            <Header>
+                <Avatar>
+                    <img src={user?.photo && user.photo} alt="Proffy" />
+                    <p>{user?.name && user.name}</p>
+                </Avatar>
+                <SignOutButton onClick={handleSignOut}>
+                    <PowerOffIcon />
+                </SignOutButton>
+            </Header>
+
+            <BodyContainer>
+                <LogoContainer>
                     <img src={logoImg} alt="Proffy" />
                     <h2>Sua plataforma de estudos online.</h2>
-                </div>
+                </LogoContainer>
 
-                <img
-                    src={landingImg}
-                    alt="Plataforma de estudos"
-                    className="hero-image" 
-                />
+                <LandingContainer>
+                    <img src={landingImg} alt="Landing"/>
+                </LandingContainer>
+            </BodyContainer>
 
-                <div className="buttons-container">
-                    <Link to="/study" className="study">
+            <Footer>
+                <WelcomeMessage>
+                    <p>Seja bem-vindo. </p>
+                    <strong>O que deseja fazer?</strong>
+                </WelcomeMessage>
+
+                <ConnectionInfo>
+                    <p>Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo" /></p>
+                </ConnectionInfo>
+                
+                <ButtonsContainer>
+                    <Link to="/study" id="study">
                         <img src={studyIcon} alt="Estudar" />
                         Estudar
                     </Link>
-
-                    <Link to="/give-classes" className="give-classes">
+                    <Link to="/give-classes" id="give-classes">
                         <img src={giveClassesIcon} alt="Dar aulas" />
                         Dar aulas
                     </Link>
-                </div>
+                </ButtonsContainer>
+            </Footer>
+        </Grid>
 
-                <span className="total-connections">
-                    Total de {totalConnections} conexões já realizadas
-                    <img src={purpleHeartIcon} alt="Coração roxo" />
-                </span>
-            </div>
-        </div>
+        // <div id="page-landing">
+        //     <button onClick={handleSignOut}>Logout</button>
+        //     <div id="page-landing-content" className="container">
+        //         <div className="logo-container">
+        //             <img src={logoImg} alt="Proffy" />
+        //             <h2>Sua plataforma de estudos online.</h2>
+        //         </div>
+
+        //         <img
+        //             src={landingImg}
+        //             alt="Plataforma de estudos"
+        //             className="hero-image" 
+        //         />
+
+        //         <div className="buttons-container">
+        //             <Link to="/study" className="study">
+        //                 <img src={studyIcon} alt="Estudar" />
+        //                 Estudar
+        //             </Link>
+
+        //             <Link to="/give-classes" className="give-classes">
+        //                 <img src={giveClassesIcon} alt="Dar aulas" />
+        //                 Dar aulas
+        //             </Link>
+        //         </div>
+
+        //         <span className="total-connections">
+        //             Total de {totalConnections} conexões já realizadas
+        //             <img src={purpleHeartIcon} alt="Coração roxo" />
+        //         </span>
+        //     </div>
+        // </div>
     )
 }
 
