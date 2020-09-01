41948020df61e84dc56ea0c2310a60e6b127cc80
HallexCosta <hallex.costa@hotmail.com>
2020-09-01 18:55:51 -0300

    Adding middlewares

---
+++ server/src/middlewares/auth.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import { promisify } from 'util'
+
+interface JWTDecoded {
+  id: number
+}
+
+export default class Authentification {  
+  async auth(request: Request, response: Response, next: NextFunction): Promise<void|Response<{}>> {
+    const { authorization } = request.headers
+
+    if (!authorization) {
+      return response.status(401).send({ error: "No token provided" })
+    } 
+
+    try {
+      const { id } = await promisify(jwt.verify)(authorization, "secret") as JWTDecoded
+
+      next()
+      return response.status(200).json({ id })
+    } catch (err) {
+      return response.status(401).json({ message: "Token invalid" })
+    }
+  } 
+}
