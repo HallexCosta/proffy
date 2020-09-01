422d038a43e0b623c8d52b43daef8aaf513b6e13
HallexCosta <hallex.costa@hotmail.com>
2020-09-01 18:48:47 -0300

    Refactoring page SignIn

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -1,86 +1,213 @@
-import React, { FormEvent } from 'react'
+import React, { useState, FormEvent } from 'react'
 import { Link } from 'react-router-dom'
+
+// import {
+//   Container,
+//   SignInContainer,
+//   ProffyContainer,
+//   ProffyContent,
+//   ProffyImage,
+//   ProffyDescription,
+//   SignInContentContainer,
+//   SignInForm,
+//   Input,
+//   Title,
+//   InputContainer,
+//   RememberContainer,
+//   RegisterInfoContainer,
+//   RegisterInfo,
+//   More,
+//   PurpleHeartIcon
+// } from './styles'
 
 import {
   Container,
-  SignInContainer,
   ProffyContainer,
-  ProffyContent,
-  ProffyImage,
-  ProffyDescription,
-  SignInContentContainer,
-  SignInForm,
-  Input,
-  Title,
-  SignInButton,
-  InputContainer,
-  RememberContainer,
-  RememberInput,
-  RegisterInfoContainer,
+  LoginContainer,
+  InputPasswordContainer,
+  RememberResetContainer,
   RegisterInfo,
-  More,
-  PurpleHeartIcon
+  Footer,
+  PurpleHeartIcon,
+  RegisterInfoMore
 } from './styles'
 
 import logo from '../../assets/images/logo.svg'
+import heartIcon from '../../assets/images/icons/purple-heart.svg'
+import showPasswordIcon from '../../assets/images/icons/show-password.svg'
+import hidePasswordIcon from '../../assets/images/icons/hide-password.svg'
 
-import HeartIcon from '../../assets/images/icons/purple-heart.svg'
+import CheckBox from '../../components/CheckBox'
+import Input from '../../components/InputV2'
+import SignInButton from '../../components/Button'
 
 import { useAuth }  from '../../contexts/auth'
 
-const SignIn: React.FC = () => {
-  const { signed, user, signIn } = useAuth()
+import { SignInUserData } from '../../services/auth'
 
-  console.log(signed)
-  console.log(user)
+export interface AuthUser {
+  name: string
+  email: string
+}
+
+const SignIn: React.FC = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+
+  // UX
+  const [isEmailActive, setEmailIsActive] = useState(false) 
+  const [isPasswordActive, setPasswordIsActive] = useState(false) 
+
+  const { signIn, remember, rememberAccount } = useAuth()
+
+  function handleShowAndHidePassword() {
+    setShowPassword(!showPassword)
+  }
+
+  function handleRememberAccount() {
+    rememberAccount()
+  }
 
   async function handleSignIn(e: FormEvent) {
     e.preventDefault()
+
+    const user: SignInUserData = {
+      email,
+      password
+    }
     
-    await signIn()
+    await signIn(user)
+  }
+
+  function handleEmail(value: string) {
+    setEmail(value)
+    value.length > 0 ? setEmailIsActive(true) :  setEmailIsActive(false)
+  }
+
+  function handlePassword(value: string) {
+    setPassword(value)
+    value.length > 0 ? setPasswordIsActive(true) : setPasswordIsActive(false)
   }
 
   return (
-    <Container>
-      <SignInContainer>
+      <Container>
         <ProffyContainer>
-          <ProffyContent>
-            <ProffyImage src={logo} />
-            <ProffyDescription>Sua plataforma de estudos online</ProffyDescription>
-          </ProffyContent>
+          <header>
+            <img src={logo} alt="Proffy Logo" />
+            <p>Sua plataforma de estudos online</p>
+          </header>
         </ProffyContainer>
-      </SignInContainer>
-      
-      <SignInContentContainer>
-        <SignInForm onSubmit={(e) => handleSignIn(e)}>
-          <Title>Fazer Login</Title>
-          <Input name="email" placeholder="E-mail" />
-          <Input name="password" placeholder="Senha" />
-          
-          <InputContainer>
-            <RememberContainer>
-              <RememberInput type="checkbox" />
-              Lembrar-me
-            </RememberContainer>
-
-            <Link to="/">Esqueci minha senha</Link>
-          </InputContainer>
-
-          <SignInButton>Entrar</SignInButton>
-        </SignInForm>
         
-        <RegisterInfoContainer>
-          <RegisterInfo>
-            Não tem conta?
-            <Link to="/signup">Cadastre-se</Link>
-          </RegisterInfo>
-          <More>
-            É de graça
-            <PurpleHeartIcon src={HeartIcon} />
-          </More>
-        </RegisterInfoContainer>
-      </SignInContentContainer>
-    </Container>
+        <LoginContainer>
+          <form onSubmit={handleSignIn}>
+            <legend>Fazer login</legend>
+
+            <Input
+              label="E-mail"
+              name="email"
+              type="email"
+              actived={isEmailActive ? true : false}
+              onChange={e => handleEmail(e.target.value)}
+              value={email}
+            />
+            <InputPasswordContainer>
+              <Input
+                label="Senha"
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                actived={isPasswordActive ? true : false}
+                onChange={e => handlePassword(e.target.value)}
+                value={password}
+              />
+              <img 
+                onClick={handleShowAndHidePassword}
+                src={showPassword ? hidePasswordIcon : showPasswordIcon}
+                alt="Show Password"
+              />
+            </InputPasswordContainer>
+            
+            <RememberResetContainer>
+              <CheckBox onClick={handleRememberAccount} label="Lembrar-me" checked={remember} />
+              <Link to="/">Esqueci minha senha</Link>
+            </RememberResetContainer>
+
+            <SignInButton
+              active={email && password ? true : false}
+            >
+              Entrar
+            </SignInButton>
+          </form>
+
+          <Footer>
+            <RegisterInfo>
+              Não tem conta?
+              <Link to="/signup">Cadastre-se</Link>
+            </RegisterInfo>
+            <RegisterInfoMore>
+              É de graça
+              <PurpleHeartIcon src={heartIcon} />
+            </RegisterInfoMore>
+          </Footer>
+        </LoginContainer>
+      </Container>
+
+    // <Container>
+    //   <SignInContainer>
+    //     <ProffyContainer>
+    //       <ProffyContent>
+    //         <ProffyImage src={logo} />
+    //         <ProffyDescription>Sua plataforma de estudos online</ProffyDescription>
+    //       </ProffyContent>
+    //     </ProffyContainer>
+    //   </SignInContainer>
+      
+    //   <SignInContentContainer>
+    //     <SignInForm onSubmit={(e) => handleSignIn(e)}>
+    //       <Title>Fazer Login</Title>
+    //       <Input
+    //         type="text"
+    //         onChange={e => setEmail(e.target.value)}
+    //         placeholder="E-mail"
+    //       />
+    //       <Input
+    //         type={showPassword ? 'text' : 'password'}
+    //         onChange={e => setPassword(e.target.value)}
+    //         placeholder="Senha"
+    //       />
+    //       <img 
+    //         onClick={handleShowAndHidePassword}
+    //         src={showPassword ? hidePasswordIcon : showPasswordIcon}
+    //         alt="Show Password"
+    //       />
+          
+    //       <InputContainer>
+    //         <RememberContainer>
+    //           <CheckBox onClick={handleRememberAccount} label="Lembrar-me" checked={remember} />
+    //         </RememberContainer>
+
+    //         <Link to="/">Esqueci minha senha</Link>
+    //       </InputContainer>
+
+    //       <SignInButton
+    //         active={email && password ? true : false}
+    //       >
+    //         Entrar
+    //       </SignInButton>
+    //     </SignInForm>
+        
+    //     <RegisterInfoContainer>
+    //       <RegisterInfo>
+    //         Não tem conta?
+    //         <Link to="/signup">Cadastre-se</Link>
+    //       </RegisterInfo>
+    //       <More>
+    //         É de graça
+    //         <PurpleHeartIcon src={heartIcon} />
+    //       </More>
+    //     </RegisterInfoContainer>
+    //   </SignInContentContainer>
+    // </Container>
   )
 }
 
--- web/src/pages/SignIn/styles.ts
+++ web/src/pages/SignIn/styles.ts
@@ -2,179 +2,310 @@ import styled from 'styled-components'
 
 import successBg from '../../assets/images/success-background.svg'
 
+// PC - ProffyContainer
+// LC - LoginContainer
+// RI - RegisterInfo
+
 export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+  display: grid;
 
-  display: flex;
-
-  color: var(--color-text-in-primary);
-`
-
-export const SignInContainer = styled.div`
-  display: flex;
-  flex: 1;
-  background: url(${successBg}) var(--color-primary);
-`
-
-export const ProffyContainer = styled.main`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
-
-export const ProffyContent = styled.div``
-
-export const ProffyImage = styled.img`
-  height: 10rem;
-`
-
-export const ProffyDescription = styled.h2`
-  max-width: 25rem;
-
-  font-weight: 500;
-  font-size: 2.4rem;
-  line-height: 4.6rem;
-  margin-top: 0.8rem;
-`
-
-export const SignInContentContainer = styled.article`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
-
-export const SignInForm = styled.form``
-
-export const Title = styled.legend`
-  font-family: 'Archivo';
-  font-size: 4.0rem;
-  font-weight: 700;
-  color: var(--color-text-title);
-  margin-bottom: 4rem;
-`
-
-export const Input = styled.input`
   width: 100%;
-  height: 5.6rem;
-  border-radius: 0.8rem;
-  background: var(--color-input-background);
-  border: 1px solid var(--color-line-in-white);
-  outline: 0;
-  padding: 0 1.6rem;
-  font: 1.6rem Archivo;
+  height: 100%;
 
-  &::placeholder {
-    color: var(--color-text-complement);
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 100vh;
+
+  grid-template-areas:
+    'PC LC'
+    'PC LC';
+`
+
+export const ProffyContainer = styled.div`
+  grid-area: PC;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+
+  background: url(${successBg}) var(--color-primary);
+
+  > header p {
+    max-width: 25rem;
+
+    font-weight: 500;
+    font-size: 2.4rem;
+    line-height: 4.6rem;
+
+    margin-top: 0.8rem;
+
+    color: var(--color-text-in-primary);
   }
-  &::-webkit-input-placeholder {
-    color: var(--color-text-complement);
+
+  > header img {
+    height: 10rem;
   }
 `
 
-export const InputContainer = styled.div`
+export const LoginContainer = styled.div`
+  grid-area: LC;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+
+  > form button {
+    margin-top: 4rem;
+  }
+
+  > form input {
+    width: 100%;
+  }
+
+  > form img {
+    position: absolute;
+    margin-top: 1.6rem;
+    right: 10%;
+    bottom: 25%;
+
+    cursor: pointer;
+  }
+
+  > form legend {
+    font-family: 'Archivo';
+    font-size: 4.0rem;
+    font-weight: 700;
+    color: var(--color-text-title);
+    margin-bottom: 4rem;
+  }
+`
+
+export const InputPasswordContainer = styled.div`
+  position: relative;
+`
+
+export const RememberResetContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
 
   margin-top: 2.4rem;
 
-  font-size: 1.4rem;
-  line-height: 2.4rem;
-
-  color: var(--color-text-complement);
-
   > div {
-    margin: 1.6rem 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  > a {
-    cursor: pointer;
-    text-decoration: none;
-    color: var(--color-text-complement);
-  }
-  
-  > a:hover {
-    color: var(--color-text-complement);
+    margin-right: 7.2rem;
   }
 `
 
-export const RememberContainer = styled.div``
-
-export const RememberInput = styled.input`
-  width: 2.4rem;
-  height: 2.4rem;
-
-  margin-right: 1.6rem;
-
-  border: 0;
-  outline: 0;
-
-  &:checked {
-    background: var(--color-secundary);
-    color: var(--color-white);
-  }
-`
-
-export const SignInButton = styled.button`
+export const Footer = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
-
-  width: 100%;
-  height: 3.6rem;
-
-  padding: 2.5rem 0;
-
-  margin-top: 4rem;
-
-  cursor: pointer;
-
-  color: var(--color-text-complement);
-  background: var(--color-button-disbabled);
-
-  font-weight: 600;
-  font: 1.6rem Archivo;
-  line-height: 2.6rem;
-
-  border-radius: .8rem;
-
-  outline: 0;
-  border: 0;
-`
-
-export const RegisterInfoContainer = styled.div`
-  width: 65%;
-  display: flex;
+  flex-direction: row;
   justify-content: space-between;
 
   margin-top: 12.8rem;
-  color: var(--color-gray);
 `
 
-export const RegisterInfo = styled.section`
+export const RegisterInfo = styled.div`  
   display: flex;
   flex-direction: column;
-  color: var(--color-text-base);
-  
-  > a {
-    color: var(--color-primary);
-  }
+  justify-content: space-between;
 
-  > a:active {
-    color: var(--color-primary);
-  }
+  margin-right: 13.6rem;
 `
 
-export const More = styled.div``
+export const RegisterInfoMore = styled.div``
 
 export const PurpleHeartIcon = styled.img`
-  margin-left: 1.2rem;
+  margin-left: 0.8rem;
 `
+
+// export const Container = styled.div`
+//   width: 100vw;
+//   height: 100vh;
+
+//   display: flex;
+
+//   color: var(--color-text-in-primary);
+// `
+
+// export const SignInContainer = styled.div`
+//   display: flex;
+//   flex: 1;
+//   background: url(${successBg}) var(--color-primary);
+// `
+
+// export const ProffyContainer = styled.main`
+//   display: flex;
+//   flex: 1;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+// `
+
+// export const ProffyContent = styled.div``
+
+// export const ProffyImage = styled.img`
+//   height: 10rem;
+// `
+
+// export const ProffyDescription = styled.h2`
+//   max-width: 25rem;
+
+//   font-weight: 500;
+//   font-size: 2.4rem;
+//   line-height: 4.6rem;
+//   margin-top: 0.8rem;
+// `
+
+// export const SignInContentContainer = styled.article`
+//   display: flex;
+//   flex: 1;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+// `
+
+// export const SignInForm = styled.form``
+
+// export const Title = styled.legend`
+//   font-family: 'Archivo';
+//   font-size: 4.0rem;
+//   font-weight: 700;
+//   color: var(--color-text-title);
+//   margin-bottom: 4rem;
+// `
+
+// export const Input = styled.input`
+//   width: 100%;
+//   height: 5.6rem;
+
+//   background: var(--color-input-background);
+//   border: 1px solid var(--color-line-in-white);
+//   border-radius: .8rem;
+
+//   outline: 0;
+
+//   padding: 0 1.6rem;
+  
+//   line-height: 2.4rem;
+//   font: 1.6rem Poppins;
+
+//   &::-webkit-input-placeholder,
+//   &::placeholder {
+//     color: var(--color-text-complement);
+//   }
+
+//   /* &:focus-within::after {
+//     width: calc(100% - 3.2rem);
+//     height: 2px;
+//     content: '';
+//     background: var(--color-primary-light);
+//     position: absolute;
+//     left: 1.6rem;
+//     right: 1.6rem;
+//     bottom: 0;
+//   } */
+
+//   & + img {
+//     position: absolute;
+//     margin-top: 1.6rem;
+//     right: 10%;
+
+//     cursor: pointer;
+//   }
+// `
+
+// export const InputContainer = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+
+//   margin-top: 2.4rem;
+
+//   font-size: 1.4rem;
+//   line-height: 2.4rem;
+
+//   color: var(--color-text-complement);
+
+//   > div {
+//     margin: 1.6rem 0;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//   }
+
+//   > a {
+//     cursor: pointer;
+//     text-decoration: none;
+//     color: var(--color-text-complement);
+//   }
+  
+//   > a:hover {
+//     color: var(--color-text-complement);
+//   }
+// `
+
+// export const RememberContainer = styled.div``
+
+// export const SignInButton = styled.button`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+
+//   width: 100%;
+//   height: 3.6rem;
+
+//   padding: 2.5rem 0;
+
+//   margin-top: 4rem;
+
+//   cursor: pointer;
+
+//   color: var(--color-text-complement);
+//   background: var(--color-button-disbabled);
+
+//   font-weight: 600;
+//   font: 1.6rem Archivo;
+//   line-height: 2.6rem;
+
+//   border-radius: .8rem;
+
+//   outline: 0;
+//   border: 0;
+// `
+
+// export const RegisterInfoContainer = styled.div`
+//   width: 65%;
+//   display: flex;
+//   justify-content: space-between;
+
+//   margin-top: 12.8rem;
+//   color: var(--color-gray);
+// `
+
+// export const RegisterInfo = styled.section`
+//   display: flex;
+//   flex-direction: column;
+//   color: var(--color-text-base);
+  
+//   > a {
+//     color: var(--color-primary);
+//   }
+
+//   > a:active {
+//     color: var(--color-primary);
+//   }
+// `
+
+// export const More = styled.div``
+
+// export const PurpleHeartIcon = styled.img`
+//   margin-left: 1.2rem;
+// `
