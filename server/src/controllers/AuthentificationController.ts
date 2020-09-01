0100461d10d5fcc6debc49c3359cc7baa9754989
HallexCosta <hallex.costa@hotmail.com>
2020-09-01 18:57:52 -0300

    Adding controller of authentification

---
+++ server/src/controllers/AuthentificationController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+import db from '../database/connection'
+
+interface User {
+  id: number
+  name: string
+  lastname: string
+  password: string
+  photo: string
+}
+
+interface RequestUser {
+  email: string
+  password: string
+}
+
+interface AuthUser {
+  token: string
+  user: {
+    name: string
+    email: string
+    photo: string
+  }
+}
+
+export default class AuthetificationController {
+  async findUserByEmailAndPassword(request: Request, response: Response): Promise<Response> {
+    const {
+      email,
+      password
+    }: RequestUser = request.body
+    
+    const user: User = await db('accounts')
+      .select('*')
+      .where('email', '=', email)
+      .first()
+
+    if (!user) {
+      return response
+      .status(401)
+      .send({
+        error: 'Incorrect email.'
+      })
+    }
+
+    const isCompatible: boolean = await bcrypt.compare(password, user.password)
+     
+    if (!isCompatible) {
+      return response
+        .status(401)
+        .send({ 
+          message: 'Incorret password.'
+        })
+    }
+
+    const token: string = jwt.sign(
+      { id: user.id },
+      "secret",
+      { expiresIn: 86400 }
+    )
+
+    const name: string = `${user.name} ${user.lastname}`
+    const photo: string = user.photo
+
+    const authUser: AuthUser = {
+     token,
+      user: {
+        name,
+        email,
+        photo
+      }
+    }
+
+    return response
+      .status(202)
+      .send(authUser)
+  }
+}
