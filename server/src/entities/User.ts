ebdd668423422839256f51d058431630a3fa800e
HallexCosta <hallex.costa@hotmail.com>
2020-09-14 14:58:21 -0300

    User entity

---
+++ server/src/entities/User.ts
@@ -0,0 +1,24 @@
+import { v4 as uuidv4 } from 'uuid'
+import { Recovery } from './Recovery'
+
+import db from '../database/connection'
+
+export class User {
+  public readonly id: string
+
+  public readonly name: string
+  public readonly lastname: string
+  public readonly email: string
+  public readonly password: string
+  public readonly photo: string
+
+  public readonly created_at: Date
+
+  constructor(props: Omit<User, 'id'>, id?: string) {
+    Object.assign(this, props)
+
+    if (!id) {
+      this.id = uuidv4()
+    }
+  }
+}
