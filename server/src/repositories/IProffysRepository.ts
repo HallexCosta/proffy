0f6676e4ac5492abacee7966c998573477b5c671
HallexCosta <hallex.costa@hotmail.com>
2020-09-30 12:11:35 -0300

    Adding method findByIds

--- server/src/repositories/IProffysRepository.ts
+++ server/src/repositories/IProffysRepository.ts
@@ -2,6 +2,7 @@ import Knex from 'knex'
 import { Proffy } from '../entities/Proffy'
 
 export interface IProffysRepository {
+  findByIds(ids: string[]): Promise<Proffy[]>
   save(proffy: Proffy): Promise<void>
   setDb(transaction: Knex.Transaction): Promise<void>
 }
