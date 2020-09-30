4a492b25063066092bf408539d39dbefc4302d51
HallexCosta <hallex.costa@hotmail.com>
2020-09-30 12:10:18 -0300

    Adding method findBySubject in contract

--- server/src/repositories/IClassesRepository.ts
+++ server/src/repositories/IClassesRepository.ts
@@ -3,6 +3,7 @@ import Knex from 'knex'
 import { Classe } from '../entities/Classe'
 
 export interface IClassesRepository {
+  findBySubject(subject: string): Promise<Classe[]>
   save(classe: Classe): Promise<void>
   setDb(transaction: Knex.Transaction): Promise<void>
 }
