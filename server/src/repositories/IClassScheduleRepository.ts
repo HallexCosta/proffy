6dbf825084965c8525d86370e542b07f0ee7cf5c
HallexCosta <hallex.costa@hotmail.com>
2020-09-30 12:11:08 -0300

    Adding method findByWeekDay, findByFrom, findByTo

--- server/src/repositories/IClassScheduleRepository.ts
+++ server/src/repositories/IClassScheduleRepository.ts
@@ -2,6 +2,9 @@ import Knex from 'knex'
 import { ClassSchedule } from '../entities/ClassSchedule'
 
 export interface IClassScheduleRepository {
+  findByWeekDay(week_day: number): Promise<ClassSchedule[]>
+  findByFrom(time: number): Promise<ClassSchedule[]>
+  findByTo(time: number): Promise<ClassSchedule[]>
   save(classSchedule: ClassSchedule): Promise<void>
   setDb(transaction: Knex.Transaction): Promise<void>
 }
